0b39a6fce51d6ceb4ca00f58627869ba23d12707
U-EPN\rgarcia <rgarcia@DESKTOP-LG5CK0C.epn.net>
2019-04-04 16:16:41 -0600

    instance

---
+++ shikainstance.c
@@ -0,0 +1,3 @@
+#include <shika.h>
+
+
--- shikainstance.h
+++ shikainstance.h
@@ -5,6 +5,7 @@
 G_DECLARE_FINAL_TYPE(ShikaInstance,shika_instance,SHIKA,INSTANCE,GObject)
 
 typedef struct _ShikaInstancePrivate ShikaInstancePrivate;
+typedef struct _ShikaLaunch ShikaLaunch;
 
 struct _ShikaInstanceClass
 {
@@ -17,6 +18,15 @@ struct _ShikaInstance
     ShikaInstancePrivate * priv;
 };
 
+
+struct _ShikaLaunch
+{
+    gchar ** env;
+    gchar * wk_dir;
+    gchar * ld_dir;
+    gchar * binary;
+    gchar * args;
+};
 
 G_BEGIN_DECLS
 
@@ -32,9 +42,37 @@ GPid            shika_instance_get_application_pid (ShikaInstance * instance);
 
 GIOStream *     shika_instance_get_stream (ShikaInstance * instance);
 
-gboolean        shika_instance_run (ShikaInstance * instance,const gchar * launcher, GError ** error);
+gboolean        shika_instance_run (ShikaInstance * instance,
+                                    ShikaLaunch * launch, 
+                                    GError ** error);
 
 gboolean        shika_instance_stop (ShikaInstance * instance);
+
+
+ShikaLaunch *       shika_launch_new (const gchar * binary,
+                                       const gchar * args);
+
+const gchar *      shika_launch_get_binary(ShikaLaunch * launch);
+
+const gchar *      shika_launch_get_args (ShikaLaunch * launch);
+
+void               shika_launch_add_env(ShikaLaunch * launch, 
+                                            const gchar * name,
+                                            const gchar * value);
+
+const gchar **     shika_launch_get_env(ShikaLaunch * launch);
+
+void               shika_launch_set_work_dir(ShikaLaunch * launch, 
+                                                const gchar * dir);
+
+const gchar *      shika_launch_get_work_dir(ShikaLaunch * launch);
+
+void               shika_launch_set_ld_dir(ShikaLaunch * launch, 
+                                                const gchar * dir);
+
+const gchar *      shika_launch_get_ld_dir(ShikaLaunch * launch);
+
+void               shika_launch_free (ShikaLaunch * launch);
 
 G_END_DECLS
 
